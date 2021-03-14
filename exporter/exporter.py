5993f9403ab2e6ca62cc4d1c993f089328e5050a
Lucas Leal <luscas.leal@gmail.com>
2021-03-14 11:21:16 -0300

    Adding carriers information to metrics

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import requests
@@ -7,7 +8,7 @@ from prometheus_client.core import GaugeMetricFamily, REGISTRY, UnknownMetricFam
 
 url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/BR/BRL/pt-BR/GRU-sky/EZE-sky/2021-09/2021-09"
 headers = {
-    'x-rapidapi-key': "key"
+    'x-rapidapi-key': os.environ['rapidapikey']
 }
 
 def export_quotes(quotes):
@@ -15,25 +16,33 @@ def export_quotes(quotes):
     response = json.loads(response.text)
     
     places = {}
+    carriers = {}
 
     for place in response.get('Places'):
-        places[place.get('PlaceId')] = place.get('CityName')
+        places[place.get('PlaceId')] = place.get('IataCode')
+
+    for carrier in response.get('Carriers'):
+        carriers[carrier.get('CarrierId')] = carrier.get('Name')
 
     for quote in response.get('Quotes'):
         origin = places.get(quote.get('OutboundLeg').get('OriginId'))
         destination = places.get(quote.get('OutboundLeg').get('DestinationId'))
-        return_date = datetime.fromisoformat(quote.get('InboundLeg').get('DepartureDate')).strftime('%d/%m/%Y')
-        outbound_date = datetime.fromisoformat(quote.get('OutboundLeg').get('DepartureDate')).strftime('%d/%m/%Y')
+        return_date = datetime.fromisoformat(quote.get('InboundLeg').get('DepartureDate'))#.strftime('%d/%m/%Y')
+        outbound_date = datetime.fromisoformat(quote.get('OutboundLeg').get('DepartureDate'))#.strftime('%d/%m/%Y')
+        duration = return_date - outbound_date
+        carrier = carriers.get(quote.get('OutboundLeg').get('CarrierIds')[0])
         quote_time = datetime.fromisoformat(quote.get('QuoteDateTime'))
         price = str(quote.get('MinPrice'))
 
-        if quote_time.date() == datetime.now().date():
-            quotes.add_metric([origin, destination, outbound_date, return_date], price)
+        quote_period = datetime.now() - quote_time
+        
+        if quote_period.days <= 1:
+            quotes.add_metric([origin, destination, outbound_date.strftime('%d/%m/%Y'), return_date.strftime('%d/%m/%Y'), str(duration.days), carrier, quote_time.strftime('%d/%m/%Y')], price)
 
 
 class CustomCollector(object):
     def collect(self):
-        labels = ['origin', 'destination', 'outbound_date', 'return_date'] #outbound = ida, inbound = volta
+        labels = ['origin', 'destination', 'outbound_date', 'return_date', 'duration', 'carrier', 'quote_time'] #outbound = ida, inbound = volta
         quotes = GaugeMetricFamily('quotes', 'Airfare Prices', labels=labels)
 
         export_quotes(quotes)
